c5eb3baad583ffa986ba0284538d25fcbfedd40c
Francisco Blázquez Matías <fran@aspgems.com>
2018-03-16 15:14:29 +0100

    Add videoinfo config to tedx

--- lib/video_info.rb
+++ lib/video_info.rb
@@ -38,7 +38,7 @@ class VideoInfo
   extend Forwardable
 
   PROVIDERS = %w[
-    Dailymotion Vkontakte Wistia
+    Dailymotion Tedx Vkontakte Wistia
     Vimeo Youtube YoutubePlaylist
   ].freeze
   PROVIDERS.each { |p| require "video_info/providers/#{p.downcase}" }
